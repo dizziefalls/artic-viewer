909f0f1e3e33e1388f3846f80323d07ef15f9a72
dizzie <dizziefalls@gmail.com>
2023-05-28 13:02:14 -0500

    Blocks out a user slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from '../features/user/userSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: userReducer
+  },
 })
+
+// Infers types from the store
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -2,20 +2,25 @@ import { createSlice } from "@reduxjs/toolkit"
 import type { PayloadAction } from "@reduxjs/toolkit"
 
 export interface UserState {
-
+  favorites: string[]
 }
 
 const initialState: UserState = {
-
+  favorites: []
 }
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-
+    addFav: (state, action: PayloadAction<string>) => {
+      state.favorites.push(action.payload)
+    },
+    removeFav: (state, action: PayloadAction<string>) => {
+      state.favorites = state.favorites.filter(fav => fav !== action.payload)
+    }
   }
 })
 
-export const {} = userSlice.actions
+export const { addFav, removeFav } = userSlice.actions
 export default userSlice.reducer
